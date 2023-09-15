168d1f106a747b329fcfe86b3e23b921c793bf35
Ajewole <oajewole@amazon.com>
2023-09-15 16:07:38 +0200

    updated

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # Hands-on 2: replace <alias> with your alias, e.g. terraform-backend-dob-elgama
-    bucket = "terraform-backend-dob-<alias>"
+    bucket = "terraform-backend-dob-oajewole"
     key    = "terraform-aws-project"
     region = "eu-central-1"
   }
