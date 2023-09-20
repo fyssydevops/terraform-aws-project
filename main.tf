feb11da254a62757535a2bd7cc8869e3d2397b35
oajewole <oajewole@amazon.com>
2023-09-20 15:29:09 +0200

    updated for module testing 2

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@
 
 # Hands-on 4: use the module from the repo terraform-aws-s3-module
 
-module "s3" {
+module "consul" {
 source = "https://github.com/fyssydevops/terraform-aws-s3-module.git"
 s3_bucket_name = "dob-sep-bucket-oajewole"
 }
