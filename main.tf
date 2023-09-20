63100c1dc69ea7580bc07b2cf52ecfe8013625a6
oajewole <oajewole@amazon.com>
2023-09-20 17:25:32 +0200

    updated with checkov

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-# resource "aws_s3_bucket" "example" {
-#   bucket_prefix = "my-tf-test-bucket-prefix"
+resource "aws_s3_bucket" "example" {
+  bucket_prefix = "my-tf-test-bucket-mewdeployment"
 
-#   tags = {
-#     Name        = "My bucket"
-#     Environment = "Dev"
-#   }
-# }
+  tags = {
+    Name        = "My bucket2"
+    Environment = "Dev"
+  }
+}
 
 # Hands-on 4: use the module from the repo terraform-aws-s3-module
 
-module "s3" {
-#source = "github.com/fyssydevops/terraform-aws-s3-module.git"
-source = "git@github.com:fyssydevops/terraform-aws-s3-module.git"
-s3_bucket_name = "dob-sep-bucket-oajewole"
-}
+# module "s3" {
+# #source = "github.com/fyssydevops/terraform-aws-s3-module.git"
+# source = "git@github.com:fyssydevops/terraform-aws-s3-module.git"
+# s3_bucket_name = "dob-sep-bucket-oajewole"
+# }
