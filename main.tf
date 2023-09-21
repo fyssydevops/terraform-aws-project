6038da1d4d63675c833172cc1d1f9e289622c5d6
oajewole <oajewole@amazon.com>
2023-09-21 15:05:57 +0200

    updated

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@
 #Hands-on 4: use the module from the repo terraform-aws-s3-module
 
 module "s3" {
-source = "https://github.com/fyssydevops/terraform-aws-s3_module.git"
-s3_bucket_name = "dob-sep-bucket-oajewole"
+  source         = "github.com/fyssydevops/terraform-aws-s3_module"
+  s3_bucket_name = "dob-sep-bucket-oajewole"
 }
