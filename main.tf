f2ffb112d0a3be61983575bf2fa37121e80aa594
oajewole <oajewole@amazon.com>
2023-09-21 12:20:48 +0200

    updated 21 Sept

--- main.tf
+++ main.tf
@@ -1,16 +1,15 @@
-resource "aws_s3_bucket" "example" {
-  bucket_prefix = "my-tf-test-bucket-mewdeployment"
+# resource "aws_s3_bucket" "example" {
+#   bucket_prefix = "my-tf-test-bucket-mewdeployment"
 
-  tags = {
-    Name        = "My bucket2"
-    Environment = "Dev"
-  }
-}
-
-# Hands-on 4: use the module from the repo terraform-aws-s3-module
-
-# module "s3" {
-# #source = "github.com/fyssydevops/terraform-aws-s3-module.git"
-# source = "git@github.com:fyssydevops/terraform-aws-s3-module.git"
-# s3_bucket_name = "dob-sep-bucket-oajewole"
+#   tags = {
+#     Name        = "My bucket2"
+#     Environment = "Dev"
+#   }
 # }
+
+#Hands-on 4: use the module from the repo terraform-aws-s3-module
+
+module "s3" {
+source = "https://github.com/fyssydevops/terraform-aws-s3_module.git"
+s3_bucket_name = "dob-sep-bucket-oajewole"
+}
